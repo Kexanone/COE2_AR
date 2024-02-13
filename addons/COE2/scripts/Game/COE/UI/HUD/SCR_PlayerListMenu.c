51435dbe5a16a07c8ee52e4ac0236a1329be9c06
Kex <kexanone@gmail.com>
2024-02-13 18:36:35 +0100

    Show Zeus icon in player list for commanders

---
+++ addons/COE2/scripts/Game/COE/UI/HUD/SCR_PlayerListMenu.c
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------------------------
+modded class SCR_PlayerListMenu : SCR_SuperMenuBase
+{
+	//------------------------------------------------------------------------------------------------
+	//! Add Zeus icon to players with commander role
+	//! --- TO DO: Use conflict commander indicator once they add it
+	override protected void UpdateGameMasterIndicator(SCR_PlayerListEntry entry, bool editorIslimited)
+	{
+		super.UpdateGameMasterIndicator(entry, editorIslimited);
+		
+		if (!editorIslimited)
+			return;
+				
+		COE_PlayerController playerController = COE_PlayerController.Cast(GetGame().GetPlayerManager().GetPlayerController(entry.m_iID));
+		if (!playerController)
+			return;
+		
+		Widget gameMasterIndicator = entry.m_wRow.FindAnyWidget(m_sGameMasterIndicatorName);
+		if (gameMasterIndicator)
+			gameMasterIndicator.SetVisible(playerController.HasPlayerRole(EPlayerRole.COE_COMMANDER));
+	}
+}
