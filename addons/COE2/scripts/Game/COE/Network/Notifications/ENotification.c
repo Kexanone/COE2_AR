56252203252ab85ae0af29a3d5159bc228ea8418
Kex <kexanone@gmail.com>
2024-02-13 14:47:48 +0100

    Add commander role notifications and hide its editor

--- addons/COE2/scripts/Game/COE/Network/Notifications/ENotification.c
+++ addons/COE2/scripts/Game/COE/Network/Notifications/ENotification.c
@@ -1,6 +1,6 @@
 modded enum ENotification
 {
-	//VOTING
+	// Voting
 	COE_VOTING_COMMANDER_IN_START, ///< Voting started to become commander (param1 = player id)
 	COE_VOTING_COMMANDER_IN_SUCCEED, ///< Voting succeeded to become commander (param1 = player id)
 	COE_VOTING_COMMANDER_IN_FAIL, ///< Voting failed to become commander (param1 = player id)
@@ -10,6 +10,9 @@ modded enum ENotification
 	COE_VOTING_COMMANDER_OUT_SUCCEED, ///< Voting succeeded to remove commander (param1 = player id)
 	COE_VOTING_COMMANDER_OUT_FAIL, ///< Voting failed to remove commander (param1 = player id)
 	COE_VOTING_COMMANDER_OUT_VOTE_CASTED, ///< Voting cast locally by player to remove other commander (param1 = player id)
-	COE_VOTING_COMMANDER_OUT_VOTE_ABSTAINED ///< Voting abstained locally by player to remove other commander (param1 = player id)
+	COE_VOTING_COMMANDER_OUT_VOTE_ABSTAINED, ///< Voting abstained locally by player to remove other commander (param1 = player id)
+	// Rights
+	COE_COMMANDER_ROLE_ASSIGNED, ///< Player got rights assigned - (param1 = GameMasterID, param2 = TargetPlayerID))
+	COE_COMMANDER_ROLE_REMOVED ///< Player got rights revoked - (param1 = GameMasterID, param2 = TargetPlayerID)
 }
 
