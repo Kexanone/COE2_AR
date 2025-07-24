3a9133ab85ed39168cd5e80cd22dba363c068eeb
Kex <kexanone@gmail.com>
2025-07-24 16:27:07 +0200

    Add enemy support settings

---
+++ addons/COE2/scripts/Game/COE/Editor/Containers/Attributes/COE_EnemyMortarCountEditorAttribute.c
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------------------------
+[BaseContainerProps(), SCR_BaseEditorAttributeCustomTitle()]
+class COE_EnemyMortarCountEditorAttribute : SCR_BaseValueListEditorAttribute
+{
+	//------------------------------------------------------------------------------------------------
+	override SCR_BaseEditorAttributeVar ReadVariable(Managed item, SCR_AttributesManagerEditorComponent manager)
+	{
+		COE_GameMode gameMode = COE_GameMode.Cast(item);
+		if (!gameMode)
+			return null;
+		
+		return SCR_BaseEditorAttributeVar.CreateFloat(gameMode.GetEnemyMortarCount());
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override void WriteVariable(Managed item, SCR_BaseEditorAttributeVar var, SCR_AttributesManagerEditorComponent manager, int playerID)
+	{
+		COE_GameMode gameMode = COE_GameMode.Cast(item);
+		if (!gameMode)
+			return;
+		
+		if (!var)
+			return;
+		
+		gameMode.SetEnemyMortarCount(Math.Round(var.GetFloat()));
+	}
+}
